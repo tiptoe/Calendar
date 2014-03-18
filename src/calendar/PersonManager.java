d4c029887f60b87f12a5da6710f26e4f2dbb7dfc
Honza <Honza@honza-f8e53fee0>
2014-03-18 08:14:49 +0100

    Doplneny kontrakty metod

--- src/calendar/PersonManager.java
+++ src/calendar/PersonManager.java
@@ -12,13 +12,45 @@ import java.util.List;
  */
 public interface PersonManager {
     
+    /**
+     * Creates Person entry in the database. Id for the entry is 
+     * automatically generated and stored into id attribute.
+     * 
+     * @param person The Person object being added to database.
+     * @throws IllegalArgumentException when person is null, or person has already 
+     * assigned id.
+     */
     void createPerson(Person person);
     
+    /**
+     * Updates the person with same ID.
+     * 
+     * @param person The Person object being updated in database.
+     * @throws IllegalArgumentException when person is null.
+     */
     void updatePerson(Person person);
      
+    /**
+     * Deletes Person entry from database.
+     * 
+     * @param person The Person object being deleted from database.
+     * @throws IllegalArgumentException when person is null.
+     */
     void deletePerson(Person person);
     
+     /**
+     * Retrieves an entity from database with matching ID.
+     * 
+     * @param id Id of the database object.
+     * @return Person object with matching ID
+     * @throws IllegalArgumentException when given id is null.
+     */
     Person getPersonById(Integer id);
     
+    /**
+     * Retrieves all Person entries from database.
+     * 
+     * @return List of all Persons
+     */
     List<Person> findAllPersons();
 }
