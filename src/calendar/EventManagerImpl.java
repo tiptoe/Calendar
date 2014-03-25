50a4eb2c07df672860a20e6668c589d2daaf2d45
Jiří <Ji@tiptoe-pc>
2014-03-25 11:08:42 +0100

    Implementation of EventManagerImplTest - CRUD tests Implementation of EventManagerImpl - CRUD operations Implementation of Event - equals, toString, hashCode Added derby libraries

--- src/calendar/EventManagerImpl.java
+++ src/calendar/EventManagerImpl.java
@@ -1,36 +1,238 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package calendar;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author Honza
+ * @author Jiri Stary
  */
 public class EventManagerImpl implements EventManager {
+    public static final Logger logger = Logger.getLogger(EventManagerImpl.class.getName());
+    
+    private Connection connection;
+    
+    public EventManagerImpl(Connection connection) {
+        this.connection = connection;
+    }
 
     @Override
-    public void createEvent(Event event) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+    public void createEvent(Event event) throws ServiceFailureException{
+            
+        if (event == null) {
+            throw new IllegalArgumentException("event is null");            
+        }
+        if (event.getId() != null) {
+            throw new IllegalArgumentException("event id is already set");            
+        }
+        if (event.getName() == null) {
+            throw new IllegalArgumentException("name is null");            
+        }
+        if (event.getStartDate() == null) {
+            throw new IllegalArgumentException("startDate is null");            
+        }
+        if (event.getEndDate() == null) {
+            throw new IllegalArgumentException("endDate is null");            
+        }
+        if ( event.getStartDate().getTime() == event.getEndDate().getTime() ) {
+            throw new IllegalArgumentException("startDate and endDate are same");            
+        }
+        if (event.getStartDate().getTime() > event.getEndDate().getTime()) {
+            throw new IllegalArgumentException("startDate is greater than endDate");            
+        }
+
+        PreparedStatement st = null;
+        try {
+            st = connection.prepareStatement(
+                    "INSERT INTO EVENT (name,startDate,endDate,note) VALUES (?,?,?,?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            st.setString(1, event.getName());
+            st.setTimestamp(2, dateToTimestamp(event.getStartDate()) );
+            st.setTimestamp(3, dateToTimestamp(event.getEndDate()));
+            st.setString(4, event.getNote());
+            int addedRows = st.executeUpdate();
+            if (addedRows != 1) {
+                throw new ServiceFailureException("Internal Error: More rows "
+                        + "inserted when trying to insert event " + event);
+            }            
+            
+            ResultSet keyRS = st.getGeneratedKeys();
+            event.setId(getKey(keyRS,event));
+            
+        } catch (SQLException ex) {
+            throw new ServiceFailureException("Error when inserting event " + event, ex);
+        } finally {
+            if (st != null) {
+                try {
+                    st.close();
+                } catch (SQLException ex) {
+                    logger.log(Level.SEVERE, null, ex);
+                }
+            }
+        } 
+    }
+    
+    private Integer getKey(ResultSet keyRS, Event event) throws ServiceFailureException, SQLException {
+        if (keyRS.next()) {
+            if (keyRS.getMetaData().getColumnCount() != 1) {
+                throw new ServiceFailureException("Internal Error: Generated key"
+                        + "retriving failed when trying to insert event " + event
+                        + " - wrong key fields count: " + keyRS.getMetaData().getColumnCount());
+            }
+            Integer result = keyRS.getInt(1);
+            if (keyRS.next()) {
+                throw new ServiceFailureException("Internal Error: Generated key"
+                        + "retriving failed when trying to insert event " + event
+                        + " - more keys found");
+            }
+            return result;
+        } else {
+            throw new ServiceFailureException("Internal Error: Generated key"
+                    + "retriving failed when trying to insert grave " + event
+                    + " - no key found");
+        }
     }
 
     @Override
     public void updateEvent(Event event) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (event == null) {
+            throw new IllegalArgumentException("event is null");            
+        }
+        if (event.getId() == null) {
+            throw new IllegalArgumentException("event id is null");            
+        }
+        if (event.getName() == null) {
+            throw new IllegalArgumentException("name is null");            
+        }
+        if (event.getStartDate() == null) {
+            throw new IllegalArgumentException("startDate is null");            
+        }
+        if (event.getEndDate() == null) {
+            throw new IllegalArgumentException("endDate is null");            
+        }
+        if ( event.getStartDate().getTime() == event.getEndDate().getTime() ) {
+            throw new IllegalArgumentException("startDate and endDate are same");            
+        }
+        if (event.getStartDate().getTime() > event.getEndDate().getTime()) {
+            throw new IllegalArgumentException("startDate is greater than endDate");            
+        }
+
+        Event original = getEventById(event.getId());
+        if (!(event.equals(original))) {
+            throw new IllegalArgumentException("Event being updated "
+                    + "is not equal to the event stored in database");
+        }
+        
+        PreparedStatement st = null;
+        try {
+            st = connection.prepareStatement(
+                    "UPDATE EVENT SET name=?, startDate=?, endDate=?, note=? WHERE ID=?");
+            st.setString(1, event.getName());
+            st.setTimestamp(2, dateToTimestamp(event.getStartDate()) );
+            st.setTimestamp(3, dateToTimestamp(event.getEndDate()));
+            st.setString(4, event.getNote());
+            st.setInt(5, event.getId());
+            int modifiedRows = st.executeUpdate();
+            if (modifiedRows != 1) {
+                throw new ServiceFailureException("Internal Error: More rows "
+                        + "modified when trying to update event " + event);
+            }
+
+        } catch (SQLException ex) {
+            throw new ServiceFailureException("Error when updating event " + event, ex);
+        } finally {
+            if (st != null) {
+                try {
+                    st.close();
+                } catch (SQLException ex) {
+                    logger.log(Level.SEVERE, null, ex);
+                }
+            }
+        }
     }
 
     @Override
     public void deleteEvent(Event event) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        if (event == null) {
+            throw new IllegalArgumentException("event is null");
+        }
+        if (event.getId() == null) {
+            throw new IllegalArgumentException("event id is null.");
+        }
+        
+        Event original = getEventById(event.getId());
+        if (!(event.equals(original))) {
+            throw new IllegalArgumentException("Internal Error: Event being deleted "
+                    + "is not equal to the event stored in database");
+        }
+        
+        PreparedStatement st = null;
+        try {
+            st = connection.prepareStatement(
+                    "DELETE FROM EVENT WHERE id=?");
+            st.setInt(1, event.getId());
+            int deletedRows = st.executeUpdate();
+            if (deletedRows != 1) {
+                throw new ServiceFailureException("Internal Error: More rows "
+                        + "deleted when trying to delete event " + event);
+            }
+
+        } catch (SQLException ex) {
+            throw new ServiceFailureException("Error when deleting event " + event, ex);
+        } finally {
+            if (st != null) {
+                try {
+                    st.close();
+                } catch (SQLException ex) {
+                    logger.log(Level.SEVERE, null, ex);
+                }
+            }
+        } 
     }
 
     @Override
-    public Event getEventById(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+    public Event getEventById(Integer id) throws ServiceFailureException {
+        PreparedStatement st = null;
+        try {
+            st = connection.prepareStatement(
+                    "SELECT id,name,startDate,endDate,note FROM event WHERE id = ?");
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            
+            if (rs.next()) {
+                Event event = resultSetToEvent(rs);
+
+                if (rs.next()) {
+                    throw new ServiceFailureException(
+                            "Internal error: More entities with the same id found "
+                            + "(source id: " + id + ", found " + event + " and " + resultSetToEvent(rs));                    
+                }            
+                
+                return event;
+            } else {
+                return null;
+            }
+            
+        } catch (SQLException ex) {
+            throw new ServiceFailureException(
+                    "Error when retrieving event with id " + id, ex);
+        } finally {
+            if (st != null) {
+                try {
+                    st.close();
+                } catch (SQLException ex) {
+                    logger.log(Level.SEVERE, null, ex);
+                }
+            }
+        } 
     }
 
     @Override
@@ -38,4 +240,21 @@ public class EventManagerImpl implements EventManager {
         throw new UnsupportedOperationException("Not supported yet.");
     }
     
+    private Event resultSetToEvent(ResultSet rs) throws SQLException {
+        Event event = new Event();
+        event.setId(rs.getInt("id"));
+        event.setName(rs.getString("name"));
+        event.setStartDate( timestampToDate(rs.getTimestamp("startDate")) );
+        event.setEndDate( timestampToDate(rs.getTimestamp("endDate")) );
+        event.setNote(rs.getString("note"));
+        return event;
+    }
+    
+    private Timestamp dateToTimestamp(Date date) {
+        return new Timestamp(date.getTime());
+    }
+    
+    private Date timestampToDate(Timestamp timestamp) {
+        return new Date(timestamp.getTime());
+    }
 }
--- test/calendar/EventManagerImplTest.java
+++ test/calendar/EventManagerImplTest.java
@@ -1,5 +1,8 @@
 package calendar;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 import org.junit.After;
@@ -15,10 +18,24 @@ import static org.junit.Assert.*;
 public class EventManagerImplTest {
     
     private EventManagerImpl manager;
+    private Connection connection;
     
     @Before
-    public void setUp() {
-        manager = new EventManagerImpl();
+    public void setUp() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:derby:memory:EventManagerTest;create=true");
+        connection.prepareStatement("CREATE TABLE EVENT ("
+                + "id int primary key generated always as identity,"
+                + "name varchar(255),"
+                + "startDate TIMESTAMP,"
+                + "endDate TIMESTAMP,"
+                + "note varchar(255))").executeUpdate();
+        manager = new EventManagerImpl(connection);
+    }
+    
+    @After
+    public void tearDown() throws SQLException {
+        connection.prepareStatement("DROP TABLE EVENT").executeUpdate();        
+        connection.close();
     }
     
     /**
@@ -40,100 +57,335 @@ public class EventManagerImplTest {
     }
     
     /**
-     * Test of createEvent method of class EventManagerImpl with wrong or tricky
-     * arguments.
+     * Tests of createEvent method of class EventManagerImpl with wrong 
+     * attributes.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testCreateEventWithWrongAttributes1() {
+        manager.createEvent(null);
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void testCreateEventWithWrongAttributes2() {
+        Date startDate = new Date(1262277038988L);
+        Date endDate = new Date(1262299038988L);
+        Event event = newTestEvent("My event", startDate, endDate, "Super awesome event!");
+        event.setId(10);
+        manager.createEvent(event);
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void testCreateEventWithWrongAttributes3() {   
+        Date startDate = new Date(1262277038988L);
+        Date endDate = new Date(1262299038988L);
+        Event event = newTestEvent(null, startDate, endDate, "Super awesome event!");
+        manager.createEvent(event);
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void testCreateEventWithWrongAttributes4() {   
+        Date endDate = new Date(8L);
+        Event event = newTestEvent("My event", null, endDate, "Super awesome event!");
+        manager.createEvent(event);
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void testCreateEventWithWrongAttributes5() {    
+        Date startDate = new Date(10L);
+        Event event = newTestEvent("My event", startDate, null, "Super awesome event!");
+        manager.createEvent(event);
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void testCreateEventWithWrongAttributes6() {    
+        Date startDate = new Date(10L);
+        Date endDate = new Date(8L);
+        Event event = newTestEvent("My event", startDate, endDate, "Super awesome event!");
+        manager.createEvent(event);
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void testCreateEventWithWrongAttributes7() {    
+        Date startDate = new Date(1262277038988L);
+        Date endDate = new Date(1262277038988L);
+        Event event = newTestEvent("My event", startDate, endDate, "Super awesome event!");
+        manager.createEvent(event);
+    }
+    
+    /**
+     * Tests of createEvent method of class EventManagerImpl with tricky 
+     * attributes.
+     * 
+     * All variants should by OK!
      */
     @Test
-    public void testCreateEventWithWrongAttributes() {
+    public void testCreateEventWithTrickyAttributes1() {    
+        Date startDate = new Date(-1262277038988L);
+        Date endDate = new Date(1262277038988L);
+        Event event = newTestEvent("My event", startDate, endDate, "Super awesome event!");
+        manager.createEvent(event);
+        Event result = manager.getEventById(event.getId()); 
+        assertNotNull(result);
+    }
+    
+    @Test
+    public void testCreateEventWithTrickyAttributes2() {    
+        Date startDate = new Date(-1262277038988L);
+        Date endDate = new Date(-1262277038966L);
+        Event event = newTestEvent("My event", startDate, endDate, "Super awesome event!");
+        manager.createEvent(event);
+        Event result = manager.getEventById(event.getId()); 
+        assertNotNull(result);
+    }
+    
+    @Test
+    public void testCreateEventWithTrickyAttributes3() {    
+        Date startDate = new Date(0L);
+        Date endDate = new Date(1262277038966L);
+        Event event = newTestEvent("My event", startDate, endDate, "Super awesome event!");
+        manager.createEvent(event);
+        Event result = manager.getEventById(event.getId()); 
+        assertNotNull(result);
+    }
+    
+    @Test
+    public void testCreateEventWithTrickyAttributes4() {    
+        Date startDate = new Date(-1262277038988L);
+        Date endDate = new Date(0L);
+        Event event = newTestEvent("My event", startDate, endDate, "Super awesome event!");
+        manager.createEvent(event);
+        Event result = manager.getEventById(event.getId()); 
+        assertNotNull(result);
+    }
+    
+    @Test
+    public void testCreateEventWithTrickyAttributes5() {    
+        Date startDate = new Date(10L);
+        Date endDate = new Date(12L);
+        Event event = newTestEvent("My event", startDate, endDate, null);
+        manager.createEvent(event);
+        Event result = manager.getEventById(event.getId()); 
+        assertNotNull(result);
+    }
+    
+    /**
+     * Tests of updateEvent method of class EventManagerImpl.
+     */
+    @Test
+    public void updateEvent() {
+       
+        Date startDate = new Date(10L);
+        Date endDate = new Date(12L);
+        Event event = newTestEvent("My event", startDate, endDate, "Super event!");
+        manager.createEvent(event);
         
-        Date startDate;
-        Date endDate;
-        Event event;
-        Event result;
+        startDate = new Date(44L);
+        endDate = new Date(66L);
+        Event event2 = newTestEvent("My other event", startDate, endDate, "Awesome event!");
+        manager.createEvent(event2);
+        
+        Integer eventId = event.getId();
+        
+        event = manager.getEventById(eventId);
+        event.setName("Event");
+        manager.updateEvent(event);        
+        assertEquals("Event", event.getName());
+        assertEquals(10L, event.getStartDate().getTime());
+        assertEquals(12L, event.getEndDate().getTime());
+        assertEquals("Super event!", event.getNote());
+        
+        event = manager.getEventById(eventId);
+        startDate = new Date(0);
+        event.setStartDate(startDate);
+        manager.updateEvent(event);        
+        assertEquals("Event", event.getName());
+        assertEquals(0, event.getStartDate().getTime());
+        assertEquals(12L, event.getEndDate().getTime());
+        assertEquals("Super event!", event.getNote());
+        
+        event = manager.getEventById(eventId);
+        endDate = new Date(1L);
+        event.setEndDate(endDate);
+        manager.updateEvent(event);        
+        assertEquals("Event", event.getName());
+        assertEquals(0, event.getStartDate().getTime());
+        assertEquals(1L, event.getEndDate().getTime());
+        assertEquals("Super event!", event.getNote());
+        
+        event = manager.getEventById(eventId);
+        event.setNote("Bad event!");
+        manager.updateEvent(event);        
+        assertEquals("Event", event.getName());
+        assertEquals(0, event.getStartDate().getTime());
+        assertEquals(1L, event.getEndDate().getTime());
+        assertEquals("Bad event!", event.getNote());
 
-        try {
-            manager.createEvent(null);
-            fail();
-        } catch (IllegalArgumentException ex) {
-            //OK
-        }
+        event = manager.getEventById(eventId);
+        event.setNote(null);
+        manager.updateEvent(event);        
+        assertEquals("Event", event.getName());
+        assertEquals(0, event.getStartDate().getTime());
+        assertEquals(1L, event.getEndDate().getTime());
+        assertNull(event.getNote());
+        
+        // Check if updates didn't affected other records
+        assertDeepEquals(event2, manager.getEventById(event2.getId()));
+    }
+    
+    /**
+     * Tests of updateEvent method of class EventManagerImpl
+     * with wrong attributes.
+     * 
+     * TestEvent:
+     * - name: "My event"
+     * - startDate: 10L
+     * - endDate: 12L
+     * - note: "Super event!"
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void updateEventWithWrongAttributes1() {
+        manager.updateEvent(null);
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void updateEventWithWrongAttributes2() {
+        Integer eventId = setUpTestEvent();    
+        
+        Event event = manager.getEventById(eventId);
+        event.setId(null);
+        manager.updateEvent(event);        
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void updateEventWithWrongAttributes3() {
+        Integer eventId = setUpTestEvent();     
+ 
+        Event event = manager.getEventById(eventId);
+        event.setId(eventId - 1);
+        manager.updateEvent(event);
 
-        startDate = new Date(1262277038988L);
-        endDate = new Date(1262299038988L);
-        event = newTestEvent("My event", startDate, endDate, "Super awesome event!");
-        event.setId(10);
-        try {
-            manager.createEvent(event);
-            fail();
-        } catch (IllegalArgumentException ex) {
-            //OK
-        }
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void updateEventWithWrongAttributes4() {    
+        Integer eventId = setUpTestEvent();
         
-        startDate = new Date(1262277038988L);
-        endDate = new Date(1262299038988L);
-        event = newTestEvent(null, startDate, endDate, "Super awesome event!");
-        try {
-            manager.createEvent(event);
-            fail();
-        } catch (IllegalArgumentException ex) {
-            //OK
-        }
+        Event event = manager.getEventById(eventId);
+        event.setName(null);
+        manager.updateEvent(event);
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void updateEventWithWrongAttributes5() {    
+        Integer eventId = setUpTestEvent();
         
-        startDate = new Date(10L);
-        endDate = new Date(8L);
-        event = newTestEvent("My event", startDate, endDate, "Super awesome event!");
-        try {
-            manager.createEvent(event);
-            fail();
-        } catch (IllegalArgumentException ex) {
-            //OK
-        }
+        Event event = manager.getEventById(eventId);
+        event.setStartDate(null);
+        manager.updateEvent(event);
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void updateEventWithWrongAttributes6() {    
+        Integer eventId = setUpTestEvent();
         
-        startDate = new Date(1262277038988L);
-        endDate = new Date(1262277038988L);
-        event = newTestEvent("My event", startDate, endDate, "Super awesome event!");
-        try {
-            manager.createEvent(event);
-            fail();
-        } catch (IllegalArgumentException ex) {
-            //OK
-        }
+        Event event = manager.getEventById(eventId);
+        event.setEndDate(null);
+        manager.updateEvent(event);
+    }
+    
+    // startDate is equal to endDate
+    @Test (expected = IllegalArgumentException.class)
+    public void updateEventWithWrongAttributes7() {    
+        Integer eventId = setUpTestEvent();
+
+        Event event = manager.getEventById(eventId);
+        Date startDate = new Date(12L);
+        event.setStartDate(startDate);
+        manager.updateEvent(event);
+    }
+    
+    // endDate is equal to startDate
+    @Test (expected = IllegalArgumentException.class)
+    public void updateEventWithWrongAttributes8() {    
+        Integer eventId = setUpTestEvent();
         
-        // these variants should be ok
-        startDate = new Date(-1262277038988L);
-        endDate = new Date(1262277038988L);
-        event = newTestEvent("My event", startDate, endDate, "Super awesome event!");
-        manager.createEvent(event);
-        result = manager.getEventById(event.getId()); 
-        assertNotNull(result);
+        Event event = manager.getEventById(eventId);
+        Date endDate = new Date(10L);
+        event.setEndDate(endDate);
+        manager.updateEvent(event);
+    }
+    
+    // startDate is greater than endDate
+    @Test (expected = IllegalArgumentException.class)
+    public void updateEventWithWrongAttributes9() {
+        Integer eventId = setUpTestEvent();
         
-        startDate = new Date(-1262277038988L);
-        endDate = new Date(-1262277038966L);
-        event = newTestEvent("My event", startDate, endDate, "Super awesome event!");
-        manager.createEvent(event);
-        result = manager.getEventById(event.getId()); 
-        assertNotNull(result);
+        Event event = manager.getEventById(eventId);
+        Date startDate = new Date(14L);
+        event.setStartDate(startDate);
+        manager.updateEvent(event);        
+    }
+    
+    // endDate is less than startDate
+    @Test (expected = IllegalArgumentException.class)
+    public void updateEventWithWrongAttributes10() { 
+        Integer eventId = setUpTestEvent();
         
-        startDate = new Date(0L);
-        endDate = new Date(1262277038966L);
-        event = newTestEvent("My event", startDate, endDate, "Super awesome event!");
-        manager.createEvent(event);
-        result = manager.getEventById(event.getId()); 
-        assertNotNull(result);
+        Event event = manager.getEventById(eventId);
+        Date endDate = new Date(8L);
+        event.setEndDate(endDate);
+        manager.updateEvent(event);
+    }
+    
+    /**
+     * Tests of deleteEvent method of class EventManagerImpl.
+     */
+    @Test
+    public void deleteEvent() {
+        Date startDate = new Date(10L);
+        Date endDate = new Date(12L);
+        Event e1 = newTestEvent("My event", startDate, endDate, "Super event!");
+        manager.createEvent(e1);
         
-        startDate = new Date(-1262277038988L);
-        endDate = new Date(0L);
-        event = newTestEvent("My event", startDate, endDate, "Super awesome event!");
-        manager.createEvent(event);
-        result = manager.getEventById(event.getId()); 
-        assertNotNull(result);
+        startDate = new Date(44L);
+        endDate = new Date(66L);
+        Event e2 = newTestEvent("My other event", startDate, endDate, "Awesome event!");
+        manager.createEvent(e2);
         
-        startDate = new Date(10L);
-        endDate = new Date(12L);
-        event = newTestEvent("My event", startDate, endDate, null);
-        manager.createEvent(event);
-        result = manager.getEventById(event.getId()); 
-        assertNotNull(result);
+        assertNotNull(manager.getEventById(e1.getId()));
+        assertNotNull(manager.getEventById(e2.getId()));
+
+        manager.deleteEvent(e1);
+        
+        assertNull(manager.getEventById(e1.getId()));
+        assertNotNull(manager.getEventById(e2.getId()));          
+    }
+    
+    /**
+     * Tests of deleteEvent method of class EventManagerImpl
+     * with wrong attributes.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void deleteEventWithWrongAttributes() {    
+        manager.deleteEvent(null);
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void deleteEventWithWrongAttributes2() {     
+        Integer eventId = setUpTestEvent();
+        
+        Event event = manager.getEventById(eventId);
+        event.setId(null);
+        manager.deleteEvent(event);
+    }
+        
+    @Test (expected = IllegalArgumentException.class)
+    public void deleteEventWithWrongAttributes3() { 
+        Integer eventId = setUpTestEvent();
+        
+        Event event = manager.getEventById(eventId);
+        event.setId(2);
+        manager.deleteEvent(event);
     }
     
     /**
@@ -163,6 +415,15 @@ public class EventManagerImplTest {
         event.setEndDate(endDate);
         event.setNote(note);
         return event;
+    }
+    
+    private int setUpTestEvent() {
+        Date startDate = new Date(10L);
+        Date endDate = new Date(12L);
+        Event event = newTestEvent("My event", startDate, endDate, "Super event!");
+        manager.createEvent(event);
+        
+        return event.getId();
     }
     
     private void assertDeepEquals(List<Event> expectedList, List<Event> actualList) {
